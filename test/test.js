8985a85488ab39d7965182b4508b56247cfc2dd9
Alois Barreras <alois@vicino.io>
2016-08-28 15:59:20 -0400

    set up tests, added logger file

--- test/test.js
+++ test/test.js
@@ -1,4 +1,11 @@
 import test from 'blue-tape';
+import fs from 'fs';
+import Infer from '../lib';
 
-test('test infers schema', t => {
+test('test infers schema for redshift', t => {
+    const readStream = fs.createReadStream('./test-data.json');
+    const options = { client: 'redshift' };
+    const infer = new Infer(options);
+    const inferredSchema = infer(readStream);
 });
+
