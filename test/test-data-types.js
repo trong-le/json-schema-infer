863acd094010747785ab22213624d8de26b29f1b
Alois Barreras <alois@vicino.io>
2016-08-29 11:40:28 -0400

    tracking file with test data types

---
+++ test/test-data-types.js
@@ -0,0 +1,32 @@
+export default {
+    'string': {
+        fn: 'string',
+        args: [],
+        dt: 'character varying',
+        priority: 0,
+    },
+    'decimal': {
+        fn: 'decimal',
+        args: [20, 5],
+        dt: 'numeric',
+        priority: 1,
+    },
+    'integer': {
+        fn: 'integer',
+        args: [],
+        dt: 'integer',
+        priority: 2,
+    },
+    'boolean': {
+        fn: 'boolean',
+        args: [],
+        dt: 'boolean',
+        priority: 3,
+    },
+    'datetime': {
+        fn: 'timestamp',
+        args: [true],
+        dt: 'timestamp without time zone',
+        priority: 4,
+    },
+}
