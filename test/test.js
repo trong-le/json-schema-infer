863e87cfe18996a7017c8fe40e31acc94eea8823
Alois Barreras <alois@vicino.io>
2016-08-29 11:40:05 -0400

    switch to dep injection for data types, removed some logs

--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@ import test from 'blue-tape';
 import fs from 'fs';
 import Infer from '../lib';
 import _ from 'highland';
+import dataTypes from './test-data-types';
 
 test('test infers schema for redshift', async t => {
     const readStream = fs.createReadStream('./test/test-data.json');
     // the stream always throws an unexpected end of input error, just ignore it till we figure out how to fix it
     const highlandStream = _(readStream).split().map(JSON.parse).errors(err => {});
-    const options = { client: 'redshift' };
+    const options = { dataTypes: dataTypes };
     const infer = new Infer(options);
     const inferredSchema = await infer.infer(highlandStream);
     const expectedSchema = [ { name: 'foo', type: 'string' }, { name: 'testFloat', type: 'decimal' }, { name: 'testInt', type: 'integer' }];
